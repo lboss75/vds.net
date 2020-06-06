774ec8c32f488aae758f7daeef8d2df488066679
Vadim Malyshev <lboss75@gmail.com>
2020-06-06 15:12:38 +0300

    create channel command

---
+++ src/client/IVySoft.VDS.Client.Cmd/CreateChannelOptions.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace IVySoft.VDS.Client.Cmd
+{
+    [Verb("channel", HelpText = "Create channel.")]
+    public class CreateChannelOptions : BaseOptions
+    {
+        [Option('t', "channel-type", Required = false, HelpText = "Channel type")]
+        public string ChannelType { get; set; } = "core.notes";
+
+        [Option('n', "channel-name", Required = true, HelpText = "Channel name")]
+        public string ChannelName { get; set; }
+    }
+}
